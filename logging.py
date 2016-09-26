4f0183939baa86f2f0f553bb34f98d16b6b71014
gaoyan0320 <18646211101@163.com>
2016-09-26 14:17:20 +0800

    20160926
    add logging

---
+++ logging.py
@@ -0,0 +1,12 @@
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename='log.txt',
+    filemode='a',
+    format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+)
+
+logging.info("info")
+logging.debug("debug")
+logging.error("error")
