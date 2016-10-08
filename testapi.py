5856f643773ad45599f33a0db98f189f8022d6ee
gaoyan0320 <18646211101@163.com>
2016-10-08 16:00:15 +0800

    20161008

--- testapi.py
+++ testapi.py
@@ -2,8 +2,8 @@ import requests
 from requests.auth import HTTPBasicAuth
 
 
-playlod = {"username": "sssssssssss",
-           "email": "sssssss@ss.com", "is_staff": "false"}
+playlod = {"username": "ssssss1ssssds",
+           "email": "ssssssds@ss.com", "is_staff": "false"}
 
 url = "http://45.116.168.137:8000/users/"
 r = requests.post(url, auth=HTTPBasicAuth(
